642d81e9573fb69dd80dc9e61078f4aa1d1043ba
abhiram050904 <abhiram050904@gmail.com>
2024-08-03 02:24:58 +0530

    q

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,11 @@ const cors = require('cors');
 app.get('/', (req, res) => {
     res.send('Hello from the server');
 });
-app.use(cors())
+
+app.use(cors());
 app.use(bodyParser.json());
-app.use('/tasks', TaskRouter)
+app.use('/tasks', TaskRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT=${PORT}`);
-})
+});
