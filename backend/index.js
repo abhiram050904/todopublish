fe11606ecdd9c337452b7e130baf5850e186c919
abhiram050904 <abhiram050904@gmail.com>
2024-08-03 02:39:04 +0530

    q

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,13 @@ const TaskRouter = require('./Routes/TaskRouter');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../public/build")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "../public", "build", "index.html"));
+    });
+  }
 app.get('/', (req, res) => {
     res.send('Hello from the server');
 });
